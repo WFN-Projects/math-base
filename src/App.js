dfdacd26b190fcae210e1b7f5232437a5021ae5f
jsiy <joeysiy8@gmail.com>
2021-01-05 12:18:14 -0800

    Connected Algolia
    Hooked up search api; used default SearchBox and Hits component temporarily

--- src/App.js
+++ src/App.js
@@ -1,27 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
 import './App.css';
-import SearchBar from "material-ui-search-bar";
 
-class App extends React.Component{
-  render(){
+const searchClient = algoliasearch(
+  '1QZ1KS7XU1',
+  '64c5bbe394f75e33424ac7026a9a5621'
+);
+
+class App extends Component {
+  render() {
     return (
       <div className="App">
         <header>Math Base</header>
         <p class="sub">A motto will go here one day...</p>
-        <SearchBar
-          class="search" 
-          placeholder="Insert a formula..."
-          style={{
-            margin: 'auto',
-            marginTop: '-250px',
-            width: '40vw'
-          }}
-        />
-        <formulas />
+        <div className="container">
+          <InstantSearch searchClient={searchClient} indexName="FINAL_FORMULAS">
+            <div className="search-panel">
+              <div className="search-panel__results">
+                <SearchBox
+                  className="searchbox"
+                  translations={{
+                    placeholder: '',
+                  }}
+                />
+                <Hits hitComponent={Hit} />
+
+                <div className="pagination">
+                  <Pagination />
+                </div>
+              </div>
+            </div>
+          </InstantSearch>
+        </div>
       </div>
     );
   }
 }
+
+function Hit(props) {
+  return (
+    <article>
+      <h1>
+        <Highlight attribute="name" hit={props.hit} />
+      </h1>
+      <p>
+        <Highlight attribute="category" hit={props.hit} />
+      </p>
+      <p>
+        <Highlight attribute="latex" hit={props.hit} />
+      </p>
+    </article>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
 
 export default App;
