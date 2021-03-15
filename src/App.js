6f3cbe2edcb0bf7fda3416dcfcf89512f81fdb67
Nicholas-Chong <48600727+Nicholas-Chong@users.noreply.github.com>
2021-03-15 13:08:14 -0400

    Integrate new modal

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import CustomHits from './components/CustomHits';
 import SearchBox from './components/CustomSearchBox';
 import Paper from "@material-ui/core/Paper";
 import { library } from '@fortawesome/fontawesome-svg-core';
+import Modal from './components/Modal'
 
 //actual search client
 const algoliaClient = algoliasearch(
@@ -39,8 +40,13 @@ const searchClient = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.modal1 = React.createRef()
+    this.modal2 = React.createRef()
+  }
+
   render() {
-    
     return (
       <div className="App">
         <div className="Title">
@@ -48,6 +54,24 @@ class App extends Component {
         </div>
         <div className="Motto">
           <p class="sub">A motto will go here one day...</p>
+        </div>
+        <div className="info">
+          <a onClick={() => {this.modal1.current.openModal()}}>About Us</a>
+          <Modal id="modal1" name="About Us" ref={this.modal1}>
+            <p>Hi there!</p>
+            <p>This project was created with ❤️ by the Western Founders Network (WFN) Projects team of 2020-21. Checkout more about WFN here!</p>
+            <p>Check out the team on LinkedIn:</p>
+            <p><a href="https://www.linkedin.com/in/jack-peplinski-054561161/" target="_blank">Jack Peplinski</a></p>
+            <p><a href="https://www.linkedin.com/in/keonjin/" target="_blank">Keon Jin</a></p>
+            <p><a href="https://www.linkedin.com/in/joseph-siy/" target="_blank">Joey Siy</a></p>
+            <p><a href="https://www.linkedin.com/in/nicholas-chong-/" target="_blank">Nicholas Chong</a></p>
+            <p><a href="https://www.linkedin.com/in/anselzeng/" target="_blank">Ansel Zeng</a></p>
+          </Modal>
+          <a onClick={() => {this.modal2.current.openModal()}}>How to Use</a>
+          <Modal id="modal2" name="How to Use" ref={this.modal2}>
+            <p>Type in the name of a formula, and MathBase will respond with formulas that have a similar name.</p>
+            <p>To copy the LaTex code for a formula, click the "Copy LaTex" button on the right of a result.</p>
+          </Modal>
         </div>
         <div className="container">
           <Paper>
